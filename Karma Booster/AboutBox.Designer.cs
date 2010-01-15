54650916faee41e0dc1ac4e3468f44a0979e3a34
Kenneth Lo <closer76@users.noreply.github.com>
2010-01-15 08:53:20 +0000

    Version 0.6.1.1 (Jan 15, 2010)
       1. Corrected some output messages.

--- Karma Booster/AboutBox.Designer.cs
+++ Karma Booster/AboutBox.Designer.cs
@@ -47,15 +47,15 @@ namespace KarmaBooster__Windows_Form_version_
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutBox));
             this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.okButton = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.labelProductName = new System.Windows.Forms.Label();
-            this.labelVersion = new System.Windows.Forms.Label();
-            this.labelCopyright = new System.Windows.Forms.Label();
-            this.labelCompanyName = new System.Windows.Forms.Label();
-            this.linkProjectHome = new System.Windows.Forms.LinkLabel();
             this.textBoxDescription = new System.Windows.Forms.TextBox();
+            this.labelCompanyName = new System.Windows.Forms.Label();
+            this.labelCopyright = new System.Windows.Forms.Label();
+            this.labelVersion = new System.Windows.Forms.Label();
+            this.labelProductName = new System.Windows.Forms.Label();
+            this.okButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkProjectHome = new System.Windows.Forms.LinkLabel();
             this.tableLayoutPanel.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -89,101 +89,6 @@ namespace KarmaBooster__Windows_Form_version_
             this.tableLayoutPanel.Size = new System.Drawing.Size(427, 333);
             this.tableLayoutPanel.TabIndex = 0;
             // 
-            // textBox1
-            // 
-            this.tableLayoutPanel.SetColumnSpan(this.textBox1, 2);
-            this.textBox1.Location = new System.Drawing.Point(3, 166);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(419, 119);
-            this.textBox1.TabIndex = 25;
-            this.textBox1.TabStop = false;
-            this.textBox1.Text = resources.GetString("textBox1.Text");
-            // 
-            // okButton
-            // 
-            this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.okButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.okButton.Location = new System.Drawing.Point(339, 296);
-            this.okButton.Name = "okButton";
-            this.okButton.Size = new System.Drawing.Size(85, 34);
-            this.okButton.TabIndex = 24;
-            this.okButton.Text = "確定(&O)";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 100);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(73, 13);
-            this.label1.TabIndex = 26;
-            this.label1.Text = "Project page: ";
-            // 
-            // labelProductName
-            // 
-            this.tableLayoutPanel.SetColumnSpan(this.labelProductName, 2);
-            this.labelProductName.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.labelProductName.Location = new System.Drawing.Point(6, 0);
-            this.labelProductName.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
-            this.labelProductName.MaximumSize = new System.Drawing.Size(0, 17);
-            this.labelProductName.Name = "labelProductName";
-            this.labelProductName.Size = new System.Drawing.Size(418, 17);
-            this.labelProductName.TabIndex = 27;
-            this.labelProductName.Text = "Karma Booster (Widnws Form Edition)";
-            this.labelProductName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // labelVersion
-            // 
-            this.tableLayoutPanel.SetColumnSpan(this.labelVersion, 2);
-            this.labelVersion.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.labelVersion.Location = new System.Drawing.Point(6, 25);
-            this.labelVersion.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
-            this.labelVersion.MaximumSize = new System.Drawing.Size(0, 17);
-            this.labelVersion.Name = "labelVersion";
-            this.labelVersion.Size = new System.Drawing.Size(418, 17);
-            this.labelVersion.TabIndex = 28;
-            this.labelVersion.Text = "Version 0.5";
-            this.labelVersion.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // labelCopyright
-            // 
-            this.tableLayoutPanel.SetColumnSpan(this.labelCopyright, 2);
-            this.labelCopyright.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.labelCopyright.Location = new System.Drawing.Point(6, 50);
-            this.labelCopyright.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
-            this.labelCopyright.MaximumSize = new System.Drawing.Size(0, 17);
-            this.labelCopyright.Name = "labelCopyright";
-            this.labelCopyright.Size = new System.Drawing.Size(418, 17);
-            this.labelCopyright.TabIndex = 29;
-            this.labelCopyright.Text = "Copyright(c) 2009 Closer Look Studio";
-            this.labelCopyright.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // labelCompanyName
-            // 
-            this.tableLayoutPanel.SetColumnSpan(this.labelCompanyName, 2);
-            this.labelCompanyName.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.labelCompanyName.Location = new System.Drawing.Point(6, 75);
-            this.labelCompanyName.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
-            this.labelCompanyName.MaximumSize = new System.Drawing.Size(0, 17);
-            this.labelCompanyName.Name = "labelCompanyName";
-            this.labelCompanyName.Size = new System.Drawing.Size(418, 17);
-            this.labelCompanyName.TabIndex = 30;
-            this.labelCompanyName.Text = "All rights reserved.";
-            this.labelCompanyName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // linkProjectHome
-            // 
-            this.linkProjectHome.AutoSize = true;
-            this.linkProjectHome.Location = new System.Drawing.Point(82, 100);
-            this.linkProjectHome.Name = "linkProjectHome";
-            this.linkProjectHome.Size = new System.Drawing.Size(213, 13);
-            this.linkProjectHome.TabIndex = 31;
-            this.linkProjectHome.TabStop = true;
-            this.linkProjectHome.Text = "https://code.google.com/p/karma-booster/";
-            this.linkProjectHome.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
-            // 
             // textBoxDescription
             // 
             this.tableLayoutPanel.SetColumnSpan(this.textBoxDescription, 2);
@@ -198,6 +103,101 @@ namespace KarmaBooster__Windows_Form_version_
             this.textBoxDescription.TabIndex = 32;
             this.textBoxDescription.TabStop = false;
             this.textBoxDescription.Text = "Don\'t do evil with Karma Booster...";
+            // 
+            // labelCompanyName
+            // 
+            this.tableLayoutPanel.SetColumnSpan(this.labelCompanyName, 2);
+            this.labelCompanyName.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelCompanyName.Location = new System.Drawing.Point(6, 75);
+            this.labelCompanyName.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
+            this.labelCompanyName.MaximumSize = new System.Drawing.Size(0, 17);
+            this.labelCompanyName.Name = "labelCompanyName";
+            this.labelCompanyName.Size = new System.Drawing.Size(418, 17);
+            this.labelCompanyName.TabIndex = 30;
+            this.labelCompanyName.Text = "All rights reserved.";
+            this.labelCompanyName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelCopyright
+            // 
+            this.tableLayoutPanel.SetColumnSpan(this.labelCopyright, 2);
+            this.labelCopyright.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelCopyright.Location = new System.Drawing.Point(6, 50);
+            this.labelCopyright.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
+            this.labelCopyright.MaximumSize = new System.Drawing.Size(0, 17);
+            this.labelCopyright.Name = "labelCopyright";
+            this.labelCopyright.Size = new System.Drawing.Size(418, 17);
+            this.labelCopyright.TabIndex = 29;
+            this.labelCopyright.Text = "Copyright(c) 2009 Closer Look Studio";
+            this.labelCopyright.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelVersion
+            // 
+            this.tableLayoutPanel.SetColumnSpan(this.labelVersion, 2);
+            this.labelVersion.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelVersion.Location = new System.Drawing.Point(6, 25);
+            this.labelVersion.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
+            this.labelVersion.MaximumSize = new System.Drawing.Size(0, 17);
+            this.labelVersion.Name = "labelVersion";
+            this.labelVersion.Size = new System.Drawing.Size(418, 17);
+            this.labelVersion.TabIndex = 28;
+            this.labelVersion.Text = "Version 0.5";
+            this.labelVersion.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelProductName
+            // 
+            this.tableLayoutPanel.SetColumnSpan(this.labelProductName, 2);
+            this.labelProductName.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelProductName.Location = new System.Drawing.Point(6, 0);
+            this.labelProductName.Margin = new System.Windows.Forms.Padding(6, 0, 3, 0);
+            this.labelProductName.MaximumSize = new System.Drawing.Size(0, 17);
+            this.labelProductName.Name = "labelProductName";
+            this.labelProductName.Size = new System.Drawing.Size(418, 17);
+            this.labelProductName.TabIndex = 27;
+            this.labelProductName.Text = "Karma Booster (Widnws Form Edition)";
+            this.labelProductName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // okButton
+            // 
+            this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.okButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.okButton.Location = new System.Drawing.Point(339, 296);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(85, 34);
+            this.okButton.TabIndex = 24;
+            this.okButton.Text = "確定(&O)";
+            // 
+            // textBox1
+            // 
+            this.tableLayoutPanel.SetColumnSpan(this.textBox1, 2);
+            this.textBox1.Location = new System.Drawing.Point(3, 166);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(419, 119);
+            this.textBox1.TabIndex = 25;
+            this.textBox1.TabStop = false;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Project page: ";
+            // 
+            // linkProjectHome
+            // 
+            this.linkProjectHome.AutoSize = true;
+            this.linkProjectHome.Location = new System.Drawing.Point(81, 100);
+            this.linkProjectHome.Name = "linkProjectHome";
+            this.linkProjectHome.Size = new System.Drawing.Size(213, 13);
+            this.linkProjectHome.TabIndex = 31;
+            this.linkProjectHome.TabStop = true;
+            this.linkProjectHome.Text = "https://code.google.com/p/karma-booster/";
+            this.linkProjectHome.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
             // AboutBox
             // 
